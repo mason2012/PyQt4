9c480c3ffe4ee087a38dcec60838be06bc16f956
mason2012 <liangzongma1997@163.com>
2016-12-15 21:29:13 +0800

    Icon.py

--- Icon.py
+++ Icon.py
@@ -1,18 +1,17 @@
 #-*- coding:utf-8 -*-
-import sys
+import sys, os
 from PyQt4 import QtGui
 
 class Icon(QtGui.QWidget):
   def __init__(self,parent=None):
     super(Icon,self).__init__(parent)
-    self.widget = QtGui.QWidget()
     self.run()
   def run(self):
-    self.widget.resize(100,300)
-    self.widget.setWindowTitle('Icon')
-    self.widget.setWindowIcon('img/Aniscree.ico')
+    self.resize(400,500)
+    self.setWindowTitle('Icon')
+    self.setWindowIcon(QtGui.QIcon('img/film.png'))
     self.show()
     
-app = QtGui.QApplicatioin(sys.argv)
+app = QtGui.QApplication(sys.argv)
 icon = Icon()
-sys.exit(app.exec_)
+sys.exit(app.exec_())
